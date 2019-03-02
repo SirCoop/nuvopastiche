008308ea94ea2ee8cfa5569ba5deae980804b1de
Gary Cooper <cooper.garym@gmail.com>
2019-03-02 17:15:02 -0500

    clean webpack plugin.

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,6 @@ const CONSTANTS = {
   API_ROOT: process.env.NODE_ENV === 'development' ? process.env.DEV_API_ROOT : process.env.PROD_API_ROOT,
 };
 
+console.log('process.env: ', process.env.NODE_ENV);
+
 export default CONSTANTS;
